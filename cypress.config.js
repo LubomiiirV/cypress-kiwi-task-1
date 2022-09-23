9fc57382d6ec428496db6a86355e60879697cbb4
Lubomir Vaclavik <vaclaviklubomir@gmail.com>
2022-09-23 09:31:54 +0200

    cypress config edit

--- cypress.config.js
+++ cypress.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl: "https://www.kiwi.com/sk",
+    baseUrl: "https://www.kiwi.com/sk/",
     chromeWebSecurity: true,
     setupNodeEvents(on, config) {
       // implement node event listeners here
